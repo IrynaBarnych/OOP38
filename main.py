d67c4f20a347619ce5bfabd77957e37fc6800e3b
IrynaBarnych <iryna.barnych@gmail.com>
2023-12-14 19:37:04 +0200

    додала код + текст завдання

--- main.py
+++ main.py
@@ -1,54 +1,54 @@
+"""Завдання 2
+Реалізація програми для додавання, видалення та
+відстеження завдань. Зберігати ці завдання у
+форматі JSON у файлі. Можливість завантаження
+раніше збережених завдань для подальшої роботи з
+ними."""
+
 import json
 import time
 
-def start_timer():
-    start_time = time.time()
-    print("Таймер запущено!")
-    return start_time
+task = {}
 
-def load_timer_state():
+def load_task():
     try:
-        with open('timer_state.json', 'r') as file:
-            timer_state = json.load(file)
+        with open("task.json", "r") as file:
+            task = json.load(file)
     except FileNotFoundError:
-        timer_state = {"start_time": 0, "elapsed_time": 0}
-    return timer_state
+        task = {}
+    return task
 
+task = load_task()
 
-def save_timer_state(timer_state):
-    with open('timer_state.json', 'w') as file:
-        json.dump(timer_state, file)
+def save_task(task):
+    with open("task.json", "w") as file:
+        json.dump(task, file)
 
+def add_task(description):
+    task[time.ctime()] = description
+    save_task(task)
 
-#стан таймера
-timer = load_timer_state()
-start_time = timer["start_time"]
-elapsed_time = timer["elapsed_time"]
-
-# Якщо таймер був запущений, продовжити відлік
-if start_time != 0:
-    elapsed_time += time.time() - start_time
-
-# Основний цикл програми
-while True:
-    print("\nМеню:")
-    print("1. Запустити/перезапустити таймер")
-    print("2. Вивести час виконання")
-    print("3. Вийти з програми")
-
-    choice = input("Виберіть опцію: ")
-
-    if choice == "1":
-        start_time = start_timer()
-        timer["start_time"] = start_time
-    elif choice == "2":
-        current_elapsed_time = elapsed_time
-        if start_time != 0:
-            current_elapsed_time += time.time() - start_time
-        print("Час виконання", current_elapsed_time)
-    elif choice == "3":
-        timer["elapsed_time"] = elapsed_time
-        save_timer_state(timer)
-        break
+def delete_task(key):
+    if key in task:
+        del task[key]
+        save_task(task)
+        print("замітка видалена")
     else:
-        print("Error")
+        print("Немає такої замітки")
+
+menu = input()
+match menu:
+    case "1":
+        print("список завдань")
+        for key, value in load_task().items():
+            print(key, value)
+
+    case "2":
+        print("Введіть замітку")
+        description = input("Введіть опис завдання")
+        add_task(description)
+    case "3":
+        print("Введіть дату таску для видалення: ")
+        key = input()
+        #key = task.keys()[0]
+        delete_task(key)
